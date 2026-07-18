2c1b78a99dcb2e617b502a9b241b920df67be6ee
Dietmar Kühl <dietmar.kuehl@me.com>
2026-07-18 23:00:32 +0100

    updated the headers and the upstream task version

---
+++ include/beman/net.hpp
@@ -0,0 +1,9 @@
+// include/beman/net.hpp                                              -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef INCLUDED_INCLUDE_BEMAN_NET
+#define INCLUDED_INCLUDE_BEMAN_NET
+
+#include <beman/net/net.hpp>
+
+#endif
